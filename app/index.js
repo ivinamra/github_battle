5ec53969d5b84be0407e91fdc9a2eee33535c586
daemon <vinamra@furlenco.com>
2019-09-21 11:47:21 +0530

    added input for battle

--- app/index.js
+++ app/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Popular from './components/popular';
+import Battle from './components/battle';
 
 
 class App extends React.Component {
   render() {
     return (
     <div className='container'>
-      <Popular />
+      <Battle />
     </div>
     )
   }
